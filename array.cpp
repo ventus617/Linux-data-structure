7f79d4e81e7877381ab4611632a086068c697e6d
ventus617 <1442001630@qq.com>
2023-03-09 17:27:15 +0800

    添加项目文件。

---
+++ array.cpp
@@ -0,0 +1,166 @@
+#include<iostream>
+#include<stdio.h>
+#include<stdlib.h>
+
+void PrintArr(int* arr, int len) {
+	for (int i = 0; i < len; i++) {
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+
+}
+
+//数组长度为n，元素为n-1内随机一个数，判断如果有重复数据则报错
+
+//计数 暴力（一个一个比较） 排序
+//在原数组基础上O（1）的空间消耗，O（n）的时间复杂消耗
+//下标交换
+int CompareArray1(int* arr,int len)
+{
+	if (arr == NULL || len <= 0)return 0;
+	//元素值与下标比较
+	int i = 0;
+	int Index = 0;
+	int temp = 0;
+	for(i;i<len;){
+		if (arr[i] == i) {
+			//处理下一个
+			i++;
+		}
+		else {
+			//检测当前元素值作为索引的位置
+			Index = arr[i];//中间值最好使用一个中间变量来接
+			if (arr[i] == arr[Index]) {
+				printf("数据重复出现");
+				return 0;
+			}
+			else {
+				temp = arr[i];
+				arr[i] = arr[Index];
+				arr[Index] = temp;
+			}
+		}
+	}
+	PrintArr(arr,len);
+	return 1;
+}
+
+int CompareArray2(int* arr, int len)
+{
+	if (arr == NULL || len <= 0)return 0;
+	//元素值与下标比较
+	int i = 0;
+	int temp = 0;
+	for (i; i < len;) {
+		if (arr[i] == i) {
+			//处理下一个
+			i++;
+		}
+		else {
+			//检测当前元素值作为索引的位置
+			if (arr[i] == arr[arr[i]]) {
+				printf("数据重复出现");
+				return 0;
+			}
+			else {
+				temp = arr[arr[i]];
+				arr[arr[i]]=arr[i];
+				arr[i] = temp;
+			}
+		}
+	}
+	PrintArr(arr, len);
+	return 1;
+}
+int CompareArray(int* arr, int len)
+{
+	if (arr == NULL || len <= 0)return 0;
+	//元素值与下标比较
+	int i = 0;
+	int temp = 0;
+	while(i<len){
+		if (arr[i] == i) {
+			//处理下一个
+			i++;
+		}
+		else {
+			//检测当前元素值作为索引的位置
+			if (arr[i] == arr[arr[i]]) {
+				printf("数据重复出现");
+				return 0;
+			}
+			else {
+				temp = arr[arr[i]];
+				arr[arr[i]] = arr[i];
+				arr[i] = temp;
+			}
+		}
+	}
+	PrintArr(arr, len);
+	return 1;
+}
+//一组数据内有一个元素仅出现一次，其余元素均出现2次，请快速找出只出现一次的元素
+//暴力计数 Hash 排序(前后均不一样) 异或（相同为0不同为1，因此相同数 异或 会抵消）
+int FindOnlyOnce(int * arr,int len) {
+	if (arr == NULL || len <= 0) return NULL;
+	int temp = arr[0];
+	for (int i = 1; i < len; i++)
+	{
+		temp=arr[i] ^ temp;
+	}
+	return temp;
+}
+//一组数据内有2个元素仅出现一次，其余元素均出现2次，请快速找出只出现一次的2个元素
+//计数 排序 
+//通过异或处理 得到一个两个不同数字的异或的值，那么在这个二进制数上，两个数的不同位是1，找一个非0位即可
+//因此按照该位数来分组，相同的数在一个组，就分出来两个不同的数了 
+void FinddifTwoNum(int* arr, int len)
+{
+	int Num1 = 0;//获取全部的异或值
+	int Num2 = 1;//分组用的位与值1
+	int DifNum1=0;
+	int DifNum2=0;
+	//整体异或
+	for (int i = 0; i < len; i++)
+	{
+		Num1 ^= arr[i];
+	}
+	//当位与值为0时，则继续寻找位与值非0的那个数 即获得了非0位
+	while ((Num1 & Num2) == 0)
+	{
+		Num2 <<= 1;
+	}
+	//分组并且每组异或得出值
+	for (int i = 0; i < len; i++) {
+		//如果相与是0，则为一组
+		if ((Num2 & arr[i])!=0) {
+			//再每组异或得值
+			DifNum2 ^= arr[i];
+		}
+		//相与不为0，则为另一组
+		else {
+			DifNum1 ^= arr[i];
+		}
+	}
+	printf("%d %d\n",Num1,Num2);
+}
+//n个元素 值1~n，元素无重复，寻找丢失的一个元素（如果新的元素排列在一个arr中）
+//异或  和减(得考虑上下限，防止内存越界出错)  数组(按下标放进去 值为0的就是空)
+int FindLostOne(int* arr, int len) {
+	if (arr == NULL || len <= 0)return;
+	int num1 = 0;
+	int num2 = 0;
+	int num3 = 0;
+	for (int i = 0; i < len; i++) {
+		num1 ^= i+1;
+		num2 ^= arr[i];
+	}
+	//增加其实也同理，一个操作
+	return num2 ^ num1;
+}
+int main(void) {
+	int arr[] = {0,1,1,2,2,3};
+	PrintArr(arr, sizeof(arr) / sizeof(int));
+	FinddifTwoNum(arr, sizeof(arr) / sizeof(int));
+	return 0;
+}
---
+++ fibonacci.cpp
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<iostream>
+using namespace std;
+unsigned int fibonacci1(unsigned int num);
+unsigned int fibonacci2(unsigned int num);
+int main(void)
+{
+	unsigned int num = 0;
+	while (1) {
+		cout << "请输入所想知的fibonacci数列的第n个数:" << endl;
+		cin >> num;
+		num = fibonacci2(num);
+		cout << "得到的fibonacci数为：" << num << endl;
+	}
+	return 0;
+}
+//如果所求数据太大，计算量太大，计算超时 例如50 调用次数近似可以用2^50-1次来看
+unsigned int fibonacci1(unsigned int num) {
+	if (num == 0)return 0;
+	if (num == 1||num==2) {
+		return 1;
+	}
+	int a = fibonacci1(num - 1) + fibonacci1(num - 2);
+	return a;
+}
+//优化fibonacci，线性的处理方式，即所需的最后一个值，是前两个值的结果，从小往大算，保存每一次的结果
+unsigned int fibonacci2(unsigned int num) {
+	if (num == 0)return 0;
+	if (num == 1 || num == 2)return 1;
+	unsigned int Numi = 0;//接f(i)的结果
+	unsigned int Numi1 = 1;//fibonacci第一个数，以及拿来接后续f(i-2)的结果
+	unsigned int Numi2 = 1;//fibonacci第二个数，以及拿来接后续f(i-1)的结果
+	//由于第一个和第一个数字已经有初值了，所以从第二个数开始
+	for (unsigned int i = 2; i < num; i++) {
+		Numi = Numi1 + Numi2;
+		Numi1 = Numi2;
+		Numi2 = Numi;
+	}
+	return Numi;
+}
